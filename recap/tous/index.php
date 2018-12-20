80b3d0d1bd7858e2fc7ddd3685976c3dcfc63ddd
thomasxd24 <tomas_tse24@me.com>
2018-12-20 16:18:08 +0100

    Update index.php

--- recap/tous/index.php
+++ recap/tous/index.php
@@ -6,7 +6,7 @@ include("../../check.php");
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>FenelonCOM | Ajouter Devoir</title>
+    <title> FenelonCOM | Ajouter Devoir</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
