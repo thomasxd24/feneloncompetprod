04c629f5e78480b878b0eb69146407e379c52682
Thomas TSE <chun_hei.tse@etu.uca.fr>
2019-01-11 18:53:06 +0100

    now

--- admin/addprof.php
+++ admin/addprof.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-// include($_SERVER['DOCUMENT_ROOT']."/connection.php");
-// include("../check.php");
+include($_SERVER['DOCUMENT_ROOT']."/connection.php");
+include("../check.php");
 
 
 // function resultToArray($result) {
