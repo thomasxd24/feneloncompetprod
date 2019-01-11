c94dc6226235d0684ffc07197f8579b40ac550e5
Thomas TSE <chun_hei.tse@etu.uca.fr>
2019-01-11 18:49:54 +0100

    hi

--- admin/addprof.php
+++ admin/addprof.php
@@ -1,8 +1,9 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT']."/connection.php");
-include("../check.php");
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
+include($_SERVER['DOCUMENT_ROOT']."/connection.php");
+include("../check.php");
+
 
 function resultToArray($result) {
     $rows = array();
@@ -11,6 +12,7 @@ function resultToArray($result) {
     }
     return $rows;
 }
+
 if (isset($_GET['query']) {
     $result = mysqli_query($db,$_GET['query']);
     $hi= resultToArray($result);
